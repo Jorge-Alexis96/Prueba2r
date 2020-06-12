85e4275883b42a4f264b2e57f4de7bc2f98b6d3a
Jorge Alexis Zanabria <63270774+ajmoose96@users.noreply.github.com>
2020-06-12 00:37:14 -0500

    Updates

---
+++ precache-manifest.a8a3c98075b37659a0ac245c06e067d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d43df410a0d935e82f92448ff7ddf5c",
+    "url": "/Prueba2r/index.html"
+  },
+  {
+    "revision": "eb45111548e6ca148079",
+    "url": "/Prueba2r/static/js/2.c6146adb.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/Prueba2r/static/js/2.c6146adb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a63c0842639cc120208",
+    "url": "/Prueba2r/static/js/main.4e0b83e2.chunk.js"
+  },
+  {
+    "revision": "76ff0c2eddb5ba6d8848",
+    "url": "/Prueba2r/static/js/runtime-main.52bcb2cd.js"
+  }
+]);
